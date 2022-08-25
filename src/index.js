596b2188ad9a02f02dcb65b1548578c10e62cbb9
Giseok <104622150+plzIABwinning@users.noreply.github.com>
2022-08-26 00:36:09 +0900

    basic function update

--- src/index.js
+++ src/index.js
@@ -1,37 +1,34 @@
-let add = document.getElementById('add_button');
+const add = document.getElementById('add_button');
 
 add.addEventListener('click', () => {
   const willDo = document.getElementsByClassName('will_do')[0];
 
   const cloneDo = willDo.cloneNode(true);
-  cloneDo.style.display = "inline-block";
+  cloneDo.style.display = "block";
 
   const main = document.getElementById('todo');
   main.appendChild(cloneDo);
 });
 
-const $ulSeletor = document.querySelector("#todo");
-
+const $ulSeletor = document.getElementById("todo");
 $ulSeletor.onclick = (e) => {
-  const nodes = [...$box.childNodes];
-  let index = nodes.indexOf(e.target);
-  if (index == -1) {
-    index = nodes.indexOf(e.target.parentElement);
-    if (index == -1) {
-      index = nodes.indexOf(e.target.parentElement.parentElement);
+  if (e.target.type === 'checkbox') {
+    const grandParent = e.target.closest('li');
+
+    if (e.target.checked) {
+      grandParent.querySelector('input[type=text]').disabled = true;
+      grandParent.querySelector('input[type=text]').style.textDecoration = 'line-through';
+    }
+    else {
+      e.target.parentElement.parentElement.querySelector('input[type=text]').disabled = false;
+      grandParent.querySelector('input[type=text]').style.textDecoration = 'none';
     }
   }
 
-  const ul = document.getElementById('todo');
-  const li = ul.getElementsByClassName('will_do');
-  const check = li[index-2].getElementsByClassName('input')[0].checked;
+  if (e.target.type === 'button') {
+    const parent = e.target.parentElement;
 
-  if (check != undefined) {
-    if (check) {
-      li[index-2].getElementsByClassName('will_do_text')[0].disabled = true;
-    }
-    else {
-      li[index-2].getElementsByClassName('will_do_text')[0].disabled = false;
-    }
+    const main = document.getElementById('todo');
+    main.removeChild(parent);
   }
 }
