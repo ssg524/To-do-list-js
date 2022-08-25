9cf1151483c0902eaf25bace474bc3e7c50913a1
Giseok <104622150+plzIABwinning@users.noreply.github.com>
2022-08-25 18:34:59 +0900

    indent update

--- src/index.js
+++ src/index.js
@@ -2,37 +2,37 @@ let add = document.getElementById('add_button');
 console.log(add);
 
 add.addEventListener('click', () => {
-    const willDo = document.getElementsByClassName('will_do')[0];
+  const willDo = document.getElementsByClassName('will_do')[0];
 
-    const cloneDo = willDo.cloneNode(true);
-    cloneDo.style.display = "inline-block";
+  const cloneDo = willDo.cloneNode(true);
+  cloneDo.style.display = "inline-block";
 
-    const main = document.getElementById('todo');
-    main.appendChild(cloneDo);
+  const main = document.getElementById('todo');
+  main.appendChild(cloneDo);
 });
 
-// document.querySelector("#todo").addEventListener('click', function (e) {});
-const $box = document.querySelector("#todo");
+const $ulSeletor = document.querySelector("#todo");
 
-$box.onclick = (e) => {
-    const nodes = [...$box.childNodes];
-    let index = nodes.indexOf(e.target);
+$ulSeletor.onclick = (e) => {
+  const nodes = [...$box.childNodes];
+  let index = nodes.indexOf(e.target);
+  if (index == -1) {
+    index = nodes.indexOf(e.target.parentElement);
     if (index == -1) {
-        index = nodes.indexOf(e.target.parentElement);
-        if (index == -1)
-            index = nodes.indexOf(e.target.parentElement.parentElement);
+      index = nodes.indexOf(e.target.parentElement.parentElement);
     }
+  }
 
-    const ul = document.getElementById('todo');
-    const li = ul.getElementsByClassName('will_do');
-    const check = li[index-2].getElementsByClassName('input')[0].checked;
+  const ul = document.getElementById('todo');
+  const li = ul.getElementsByClassName('will_do');
+  const check = li[index-2].getElementsByClassName('input')[0].checked;
 
-    if (check != undefined) {
-        if (check) {
-            li[index-2].getElementsByClassName('will_do_text')[0].disabled = true;
-        }
-        else {
-            li[index-2].getElementsByClassName('will_do_text')[0].disabled = false;
-        }
+  if (check != undefined) {
+    if (check) {
+      li[index-2].getElementsByClassName('will_do_text')[0].disabled = true;
     }
+    else {
+      li[index-2].getElementsByClassName('will_do_text')[0].disabled = false;
+    }
+  }
 }
